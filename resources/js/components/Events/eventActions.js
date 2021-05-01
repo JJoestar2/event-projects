b587e6f24a93de4f2482cdd5a68bae0f0891b800
Johnny <gavriltsiv250@gmail.com>
2021-05-01 13:36:14 +0300

    -Created and stylized event details view.

--- resources/js/components/Events/eventActions.js
+++ resources/js/components/Events/eventActions.js
@@ -34,7 +34,7 @@ export const sortEvents = (filter, events) => (dispatch) => {
         events.sort((a,b) => (a.id > b.id ? 1 : -1))
     }
 
-    return dispatch(sortEventsAction(filter, events));bl
+    return dispatch(sortEventsAction(filter, events));
 };
 
 const grabAllEvents = data => ({
