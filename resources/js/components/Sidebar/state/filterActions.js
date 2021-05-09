5f410a0726811dcf7919af6dea742d5c9a847756
Johnny <gavriltsiv250@gmail.com>
2021-05-09 15:57:37 +0300

    Multiple filters on redux.

--- resources/js/components/Sidebar/state/filterActions.js
+++ resources/js/components/Sidebar/state/filterActions.js
@@ -1,4 +1,4 @@
-import {GET_ALL_CATEGORIES, GET_ALL_TYPES} from "./filterTypes";
+import {GET_ALL_CATEGORIES, GET_ALL_TYPES, SET_FILTER, REMOVE_FILTER, REMOVE_ALL_FILTERS} from "./filterTypes";
 
 import {getAllTypes} from "../../../services/TypeService";
 import {getAllCategories} from "../../../services/CategoryService";
@@ -17,6 +17,37 @@ export const getFilterTypes = () => {
         });
     };
 };
+
+export const addFilter = (type, value) => (dispatch) => {
+    return dispatch(setFilter(type, value));
+};
+
+export const deleteFilter = (type) => (dispatch) => {
+    return dispatch(removeFilter(type));
+};
+
+export const removeAllFilters = (dispatch) => {
+    return dispatch(clearFilters());
+};
+
+const setFilter = (type, value) => ({
+    type: SET_FILTER,
+    payload: {
+        type: type,
+        value: value
+    }
+});
+
+const removeFilter = (type) => ({
+    type: REMOVE_FILTER,
+    payload: {
+        type: type
+    }
+});
+
+const clearFilters = () => ({
+    type: REMOVE_ALL_FILTERS
+});
 
 const grabAllCategories = data => ({
     type: GET_ALL_CATEGORIES,
--- resources/js/components/Sidebar/state/filterReducer.js
+++ resources/js/components/Sidebar/state/filterReducer.js
@@ -1,8 +1,9 @@
-import {GET_ALL_TYPES, GET_ALL_CATEGORIES} from "./filterTypes";
+import {GET_ALL_TYPES, GET_ALL_CATEGORIES, SET_FILTER, REMOVE_FILTER, REMOVE_ALL_FILTERS} from "./filterTypes";
 
 const initialState = {
    eventCategories: [],
    eventTypes: [],
+   eventFilters:[]
 };
 
 function filterReducer(state = initialState, action) {
@@ -19,6 +20,27 @@ function filterReducer(state = initialState, action) {
                 ...state,
                 eventTypes: action.payload
             };
+
+        case SET_FILTER:
+            return {
+                ...state,
+                eventFilters: [
+                    ...state.eventFilters,
+                    action.payload
+                ]
+            }
+
+        case REMOVE_FILTER:
+            return {
+                ...state,
+                eventFilters: state.eventFilters.filter(item => item.type != action.payload.type)
+            }
+
+        case REMOVE_ALL_FILTERS:
+            return {
+                ...state,
+                eventFilters: []
+            }
 
         default:
             return state;
